247bdf2184d8180e30f82e8dc39358d735dce82d
jaromaco <jvr.roma@outlook.es>
2025-03-16 09:48:32 +0100

    continuamos

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './Logo.png';
 import './App.css';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
 import Home from './components/Home';
@@ -6,11 +5,12 @@ import About from './components/About';
 import Services from './components/Services';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
-
+import Hero from './components/Hero';
 
 const router = createBrowserRouter(createRoutesFromElements(
   
   <Route path="/" element={<Home />}>
+    <Route index element={<Hero />} />
     <Route path="about" element={<About />} />
     <Route path="services" element={<Services />} />
     <Route path="portfolio" element={<Portfolio />} />
---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Hero.css";
+
+
+export default function Hero () {
+  return (
+    <div className="hero">
+      <h1>Javier Roma Corrales</h1>
+      <p>GIS Developer</p>
+    </div>
+  );
+}
